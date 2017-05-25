1dd35dd2cc53165ffa666855ba7c2f3454f5065e
Sebastian Cheung <sebastian_cheung@yahoo.com>
2017-05-25 01:31:54 +0100

    fixed exception returns instead of print

--- api/sportr_sample.py
+++ api/sportr_sample.py
@@ -10,7 +10,7 @@ def get_url(user_url):
     try:
         r = requests.get(user_url)
         r.raise_for_status()
-        soup = BeautifulSoup(r.text, "html.parser")
+        soup = BeautifulSoup(r.content, "html.parser")
         url = soup.findAll(attrs={"property": "og:url"})[0]['content'].encode('utf-8')
         headline = soup.find('title').string
         caption = soup.findAll("figcaption", {"class": "sp-media-asset__caption gel-brevier "})[0].string
@@ -21,17 +21,14 @@ def get_url(user_url):
 
         return json.dumps([{"url": url, "headline": headline, "images": [{"url": images, "caption": caption}]}])
     except (requests.exceptions.RequestException, IndexError) as e:
-        print e
+        # print 'Type(e) is :', type(e)
+        return e
 
 
 def start():
     while True:
         s = get_url(raw_input("enter url: "))
         print s, "\n"
-
-
-def multiply(x, y):
-    return x * y
 
 
 if __name__ == '__main__':
--- api/tests/test_sportr_sample.py
+++ api/tests/test_sportr_sample.py
@@ -1,34 +1,38 @@
 import unittest
-from api.sportr_sample import get_url, start, multiply
+from api.sportr_sample import get_url, start
+from requests import HTTPError
 
 
 class TestGetUrl(unittest.TestCase):
     def test_get_valid_url(self):
         self.assertEqual(
-            '[{"url": "http://www.bbc.co.uk/sport/tennis/37268846", "headline": "US Open 2016: Novak Djokovic beats Kyle Edmund in fourth round - BBC Sport", "images": [{"url": "http://ichef.bbci.co.uk/onesport/cps/624/cpsprodpb/2AC8/production/_91025901_djokovicedmund.jpg", "caption": "Edmund was unable to make much of an impression against Djokovic"}]}]',
+            '[{"url": "http://www.bbc.co.uk/sport/tennis/37268846", '
+            '"headline": "US Open 2016: Novak Djokovic beats Kyle Edmund in fourth round - BBC Sport", '
+            '"images": [{"url": "http://ichef.bbci.co.uk/onesport/cps/624/cpsprodpb/2AC8/production/_91025901_djokovicedmund.jpg", '
+            '"caption": "Edmund was unable to make much of an impression against Djokovic"}]}]',
             get_url('http://www.bbc.co.uk/sport/tennis/37268846'))
 
 
 class TestGetBadUrl(unittest.TestCase):
     def test_get_invalid_url(self):
-        self.assertEqual(None, get_url('http://www.bbc.co.uk/sport/tennis/xxx'))
+        self.failureException(HTTPError(u'404 Client Error: Not Found for url: http://www.bbc.co.uk/sport/tennis/xxx',),
+                         get_url('http://www.bbc.co.uk/sport/tennis/xxx'))
 
 
 class TestGetRandomUrl(unittest.TestCase):
     def test_get_random_text(self):
-        self.assertEqual(None, get_url('somerandomtext'))
+        self.failureException("Invalid URL 'xyz': No schema supplied. Perhaps you meant http://xyz?", get_url('xyz'))
+
+
+class TestGetValidButNoIndexUrl(unittest.TestCase):
+    def test_get_valid_but_not_valid_text(self):
+        self.failureException(IndexError('list index out of range',), get_url('http://www.google.com'))
 
 
 # class TestStart(unittest.TestCase):
 #     def test_start(self):
 #         # self.assertEqual(expected, start())
 #         assert False  # TODO: implement your test here
-
-
-class TestMultiply(unittest.TestCase):
-    def test_multiply(self):
-        self.assertEqual(6, multiply(2, 3))
-        # assert False # TODO: implement your test here
 
 
 if __name__ == '__main__':
